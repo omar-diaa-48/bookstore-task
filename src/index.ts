02f29b95dc8d3630fc319f2f2f00baa98161513f
Omar <omar@kamelizer.com>
2022-11-27 16:34:46 +0200

    express initial setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello world!')
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT;
+const NODE_ENV = process.env.NODE_ENV;
+
+app.listen(PORT, () => console.log(`Backend is up and running on port ${PORT}, with environment ${NODE_ENV}`))
