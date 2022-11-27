2be9d597b186ff8f7c0ea8eba501b6a88dd5c83c
Omar <omar@kamelizer.com>
2022-11-27 17:10:43 +0200

    add sequelize and mysql dependencies

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,9 @@ dotenv.config();
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const PORT = process.env.PORT;
 const NODE_ENV = process.env.NODE_ENV;
 
